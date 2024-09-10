d3bb5465d7a20f119c78cc90586a63f669ff89cb
drorgolan <drorgolan76@gmail.com>
2024-09-10 12:40:54 +0300

    Added all user and auth user logic . working user login regiser and logout

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,15 +4,27 @@ use App\Http\Controllers\CustomerController;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware(Authenticate::using('sanctum'));
+//Customer routes
+//Route::middleware('auth:sanctum')->group(function () {
+//    Route::apiResource('customers', CustomerController::class);
+//});
 
-
+Route::middleware('auth:sanctum')->group(function () {
 Route::get('/customers', [CustomerController::class, 'index']);
 Route::post('/customers', [CustomerController::class, 'store']);
 Route::get('/customers/{id}', [CustomerController::class, 'show']);
 Route::put('/customers/{id}', [CustomerController::class, 'update']);
 Route::delete('/customers/{id}', [CustomerController::class, 'destroy']);
+});
 
+
+//User routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('user', [AuthController::class, 'user']);
+});
